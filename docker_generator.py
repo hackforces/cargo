361ba909da7efc8d3934770979cc9ea87d8deee3
den-isk1995 <den-isk1995@mail.ru>
2017-06-25 13:43:16 +0700

    Fixed bugs, improved code style. Resolved issues. Writing to Dockerfile at the finish.

--- docker_generator.py
+++ docker_generator.py
@@ -1,55 +1,55 @@
 from cement.core.foundation import CementApp
-from cement.core import hook
 from cement.utils.misc import init_defaults
 import json
 import os
 import re
 
+# This variable for forming Dockerfile before writing it to real file
+dockerstrings = []
+
 # Function for adding ssh-server
 # Based on https://docs.docker.com/engine/examples/running_ssh_service/
 # Have only 'root' user and you can set 'passowrd'
 # But after using this function, please, read https://habrahabr.ru/company/infopulse/blog/237737/
-def sshd_config(dockerfile, install, password):
-    dockerfile.write("RUN {} openssh-server\n".format(install))
-    dockerfile.write("RUN mkdir /var/run/sshd\n")
+def sshd_config(install, password):
+    dockerstrings.append("RUN {} openssh-server\n".format(install))
+    dockerstrings.append("RUN mkdir /var/run/sshd\n")
 
-    #? May be change password before sshd_config
-    dockerfile.write("RUN echo 'root:{}' | chpasswd\n".format(password))
+    # TODO: May be change password before sshd_config
+    dockerstrings.append("RUN echo 'root:{}' | chpasswd\n".format(password))
 
     # This string replace all rules in sshd_config
     # It's setting by default only for testing
     # I recommend change this default settings in future.
-    dockerfile.write("RUN sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config\n")
+    dockerstrings.append("RUN sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config\n")
 
     # For ssh docker will use 22th port
-    dockerfile.write("EXPOSE 22\nCMD [\"/usr/sbin/sshd\", \"-D\"]\n")
+    dockerstrings.append("EXPOSE 22\nCMD [\"/usr/sbin/sshd\", \"-D\"]\n")
 
-def telnetd_config(dockerfile, install, password):
-    dockerfile.write("RUN {} telnetd\n".format(install))
+def telnetd_config(install, password):
+    dockerstrings.append("RUN {} telnetd\n".format(install))
 
-    #? May be change password before telnetd_config
-    dockerfile.write("RUN echo 'root:{}' | chpasswd\n".format(password))
-    dockerfile.write("EXPOSE 23\nCMD [\"/usr/sbin/sshd\", \"-D\"]\n")
+    # TODO: May be change password before telnetd_config
+    dockerstrings.append("RUN echo 'root:{}' | chpasswd\n".format(password))
+    dockerstrings.append("EXPOSE 23\nCMD [\"/usr/sbin/sshd\", \"-D\"]\n")
 
 # Function for checking of existence some package in
 # repository of OS
 def check_existence_in_repository(os_name, utils):
-    dirlist = os.listdir(os.path.dirname(__file__) + "/os/")
-    if os_name not in dirlist:
+
+    if os_name not in os.listdir(os.path.dirname(__file__) + "/os/"):
         return None
     # TODO: This checking works only in Unix systems
     # If file less than 3 (grabber.py , ., ..), that list of files in
     # OS's repositories doesn't downloaded yet
     path = os.path.dirname(__file__) + "/os/{}/".format(os_name)
-    directories = (os.listdir(path))
-    if len(directories) < 2:
+    if len(os.listdir(path)) < 2:
 
         # Executing script for parsing all utils's names and versions in repositories
         os.execl("python", path + "grabber.py")
-        directories = (os.listdir(path))
 
     # Any directory's name corresponds of repository's version
-    for file in directories:
+    for file in os.listdir(path):
 
         filepath = path + file
         if os.path.isdir(filepath):
@@ -78,9 +78,9 @@ def add_external_file():
         answer = input("Please, enter path (or link) of your file and destination path in your Docker: ")
 
     if local_machine:
-        dockerfile.write("COPY {} {}\n".format(answer[0], answer[1]))
+        dockerstrings.append("COPY {} {}\n".format(answer[0], answer[1]))
     else:
-        dockerfile.write("ADD {} {}\n".format(answer[0], answer[1]))
+        dockerstrings.append("ADD {} {}\n".format(answer[0], answer[1]))
 
     return answer
 
@@ -99,28 +99,28 @@ def language_interactive(os_installer, language):
         files = add_external_file()
 
     if 'python' in language.lower():
-        dockerfile.write("RUN {} -y install python-pip\n".format(os_installer))
-        dockerfile.write("RUN p=pwn && cd {}".format(os.path.dirname(files[1])))
+        dockerstrings.append("RUN {} python-pip\n".format(os_installer))
+        dockerstrings.append("RUN p=pwn && cd {}".format(os.path.dirname(files[1])))
         if "3" in language:
-            dockerfile.write(" && pip3 install -r ./requests.txt ")
+            dockerstrings.append(" && pip3 install -r ./requests.txt ")
         else:
-            dockerfile.write(" && pip install -r ./requests.txt ")
+            dockerstrings.append(" && pip install -r ./requests.txt ")
 
-        dockerfile.write("&& cd $p\n")
+        dockerstrings.append("&& cd $p\n")
 
     if 'php' in language.lower():
-        dockerfile.write("RUN curl -sS https://getcomposer.org/installer | sudo {} -- --install-dir=/usr/local/bin --filename=composer\n".format(language))
+        dockerstrings.append("RUN curl -sS https://getcomposer.org/installer | sudo {} --install-dir=/usr/local/bin --filename=composer\n".format(language))
         # As php's composer make install all references from composer.json in current directory
         # we need to enter to this directory and return come back
-        dockerfile.write("RUN p=pwn && cd {} && composer install && cd $p\n".format(os.path.dirname(files[1])))
+        dockerstrings.append("RUN p=pwn && cd {} && composer install && cd $p\n".format(os.path.dirname(files[1])))
 
     if 'node' in language.lower():
-        dockerfile.write("RUN {} -y install npm\n".format(os_installer))
+        dockerstrings.append("RUN {} npm\n".format(os_installer))
         # As nodejs's npm make install all references from package.json in current directory
         # we need to enter to this directory and return come back
-        dockerfile.write("RUN p=pwn && cd {} && npm install && cd $p\n".format(os.path.dirname(files[1])))
+        dockerstrings.append("RUN p=pwn && cd {} && npm install && cd $p\n".format(os.path.dirname(files[1])))
 
-def database_interactive(os_installer, database):
+def database_interactive(database):
     answer = ''
     while answer.lower() not in "yn":
         answer = input("This file on local machine? [Y/N]: ")
@@ -137,18 +137,18 @@ def database_interactive(os_installer, database):
     # https://stackoverflow.com/questions/25920029/setting-up-mysql-and-importing-dump-within-dockerfile
     # https://stackoverflow.com/questions/4546778/how-can-i-import-a-database-with-mysql-from-terminal
     if "mysql" in database.lower():
-        dockerfile.write("RUN /bin/bash -c \"/usr/bin/mysqld_safe &\" && \
+        dockerstrings.append("RUN /bin/bash -c \"/usr/bin/mysqld_safe &\" && \
                             sleep 5 && \
                             mysql -u {} -e \"CREATE DATABASE {}\" && \
                             mysql -u {} -p {} {} < {}\n".format(username, database_name,
                             username, password, database_name, files[1]))
 
     if "postgresql" in database.lower():
-        dockerfile.write("RUN psql -U {} {} < {}\n".format(username, database_name, files[1]))
+        dockerstrings.append("RUN psql -U {} {} < {}\n".format(username, database_name, files[1]))
 
     # https://docs.mongodb.com/manual/tutorial/backup-and-restore-tools/
     if "mongodb" in database.lower():
-        dockerfile.write("RUN mongorestore {}".format(files[1]))
+        dockerstrings.append("RUN mongorestore {}".format(files[1]))
 
 
 
@@ -169,8 +169,6 @@ defaults = init_defaults('docker_generator')
 defaults['docker_generator']['debug'] = False
 defaults['docker_generator']['some_param'] = 'some value'
 
-dockerfile = open("Dockerfile","w")
-
 # define any hook functions here
 def my_cleanup_hook(app):
     pass
@@ -183,7 +181,8 @@ class Cargo(CementApp):
         hooks = [
             ('pre_close', my_cleanup_hook),
         ]
-        langugages = ['python', 'go', 'php', 'nodejs', 'c', 'c++', 'java', 'ruby', 'rust']
+        languages = ['python', 'go', 'php', 'js', 'c', 'c++', 'java', 'ruby', 'rust']
+        restart = ['on-failure', 'always', 'unless-stopped']
 with Cargo() as app:
     # add arguments to the parser
     app.args.add_argument('-o', '--os', action='store',
@@ -225,15 +224,15 @@ with Cargo() as app:
     # Choice Operation System and set install
     if app.pargs.os:
         choicen_os = app.pargs.os#choise_os_version(app.pargs.os.lower())
-        dockerfile.write("FROM {}\n".format(choicen_os))
+        dockerstrings.append("FROM {}\n".format(choicen_os))
         app.log.info("Received option: os => {}".format(choicen_os))
 
         if "ubuntu" in app.pargs.os.lower() or "debian" in app.pargs.os.lower():
             install = "apt-get -qq -y install"
         elif "arch" in app.pargs.os.lower():
-            install = "pacman -S"
+            install = "pacman -S install"
         elif "centos" in app.pargs.os.lower():
-            install = "yum"
+            install = "yum install"
         else:
             print("We are cannot support this OS: {}".format(app.pargs.os))
 
@@ -241,7 +240,7 @@ with Cargo() as app:
         operation_system = app.pargs.os
 
 
-    dockerfile.write(maintainer)
+    dockerstrings.append(maintainer)
 
     # Make install some language interpretator, independently from OS
     # Language pull: Rust, Go, Ruby, Python, PHP, Javascript, C, C++, Java
@@ -264,7 +263,7 @@ with Cargo() as app:
             language_packet = check_existence(operation_system, "python")
         if "php" in app.pargs.language.lower():
             language_packet = check_existence(operation_system, "php")
-        if "javascript" in app.pargs.language.lower():
+        if "js" in app.pargs.language.lower():
             language_packet = check_existence(operation_system, "nodejs")
         if "c" in app.pargs.language.lower():
             language_packet = check_existence(operation_system, "gcc")
@@ -275,12 +274,12 @@ with Cargo() as app:
             language_packet = check_existence(operation_system, "default-jre")
         if "rust" in app.pargs.language.lower():
             print("This is specify language, which are not in {}'s repository. Default value is used".format(operation_system))
-            dockerfile.write("RUN {} curl".format(install))
-            dockerfile.write("RUN {} curl https://sh.rustup.rs -sSf | sh\n".format(install))
+            dockerstrings.append("RUN {} curl".format(install))
+            dockerstrings.append("RUN curl https://sh.rustup.rs -sSf | sh\n")
             specify_language = 1
 
         if not specify_language:
-            dockerfile.write("RUN {} {}\n".format(install, language_packet))
+            dockerstrings.append("RUN {} {}\n".format(install, language_packet))
 
     # Make a choice of Databases
     # Pull of DBs: MySQL, PostgreSQL, MongoDB
@@ -302,7 +301,10 @@ with Cargo() as app:
         if "mongodb" in app.pargs.database.lower():
             database = check_existence(operation_system, "mongodb-org")
 
-        dockerfile.write("RUN {}\n".format(install))
+        dockerstrings.append("RUN {} {}\n".format(install, database))
+
+        # Interactive mode for import DB to Docker
+        database_interactive(database)
 
     # Make a choice of ports, using by Docker
     if app.pargs.ports:
@@ -312,7 +314,7 @@ with Cargo() as app:
             ports = re.findall(r"[, ]*(\d)[, ]*[^-]", app.pargs.ports)
             for port in ports:
                 if port:
-                    dockerfile.write("EXPOSE {}\n".format(str(port)))
+                    dockerstrings.append("EXPOSE {}\n".format(str(port)))
 
         # If range is given
         if "-" in app.pargs.ports:
@@ -321,7 +323,7 @@ with Cargo() as app:
             i = 0
             while i < len(ports):
                 for port in range(int(ports[i][0]), int(ports[i][1])+1):
-                    dockerfile.write("EXPOSE {}\n".format(str(port)))
+                    dockerstrings.append("EXPOSE {}\n".format(str(port)))
                 i +=1
 
 
@@ -329,11 +331,11 @@ with Cargo() as app:
 
     # Make active ssh-server and setting root's password
     if app.pargs.ssh:
-        sshd_config(dockerfile, install, app.pargs.ssh)
+        sshd_config(install, app.pargs.ssh)
 
     # Make active telnet-server and setting root's password
     if app.pargs.telnet:
-        sshd_config(dockerfile, install, app.pargs.telnet)
+        telnetd_config(install, app.pargs.telnet)
 
     # List of docker restart conditions
     restart_conditions = ['on-failure', 'always', 'unless-stopped']
@@ -341,7 +343,7 @@ with Cargo() as app:
     # Make choice container restart condition
     if app.pargs.restart:
         if app.pargs.restart.lower() in restart_conditions:
-            dockerfile.write("CMD [\"--restart\", \"{}\"]\n".format(app.pargs.restart.lower()))
+            dockerstrings.append("CMD [\"--restart\", \"{}\"]\n".format(app.pargs.restart.lower()))
 
     # Make a choice of volume directory
     if app.pargs.volume:
@@ -352,7 +354,7 @@ with Cargo() as app:
             choice = input("Choose next common(shared) directory (or \'Q\' for finish choice): ")
             if choice.lower() is "q":
                 continue
-            dockerfile.write("VOLUME [\"{}\"]\n".format(choice))
+            dockerstrings.append("VOLUME [\"{}\"]\n".format(choice))
 
     # Make a choice of addition files to docker image
     if app.pargs.add:
@@ -362,7 +364,7 @@ with Cargo() as app:
         while choice_paths_from.lower() != "q":
             choice_paths_from = input("Choose next file (or \'Q\' for finish choice): ")
             choice_paths_to = input("Choose destination directory: ")
-            if choice_paths_from.lower() == "q":
+            if choice_paths_from.lower() is "q":
                 continue
 
             if len(choice_paths_from) == 0 or len(choice_paths_to) == 0:
@@ -372,13 +374,15 @@ with Cargo() as app:
             choice = ''
             while choice not in "yn":
                 choice = input("Is this file on your local machine? [Y/N]: ")
-                if choice.lower() == "y":
-                    dockerfile.write("COPY \"{}\" \"{}\"\n".format(choice_paths_from, choice_paths_to))
-                elif choice.lower() == "n":
-                    dockerfile.write("ADD \"{}\" \"{}\"\n".format(choice_paths_from, choice_paths_to))
+                if choice.lower() is "y":
+                    dockerstrings.append("COPY \"{}\" \"{}\"\n".format(choice_paths_from, choice_paths_to))
+                elif choice.lower() is "n":
+                    dockerstrings.append("ADD \"{}\" \"{}\"\n".format(choice_paths_from, choice_paths_to))
 
     if app.pargs.workdir:
-        dockerfile.write("WORKDIR {}\n".format(app.pargs.workdir))
+        dockerstrings.append("WORKDIR {}\n".format(app.pargs.workdir))
 
-
+    dockerfile = open("Dockerfile", "w")
+    print dockerstrings
+    dockerfile.writelines(dockerstrings)
     dockerfile.close()

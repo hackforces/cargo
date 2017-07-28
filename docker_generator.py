03779f46e311a9c8d74d9cc9d143d43fd9bf1dbf
den-isk1995 <den-isk1995@mail.ru>
2017-07-28 21:28:21 +0700

    Fuck off interactive mode! Made this program more usable.

--- docker_generator.py
+++ docker_generator.py
@@ -34,43 +34,81 @@ database_default = {
 # This variable for forming Dockerfile before writing it to real file
 dockerstrings = []
 
+# This files will not add to Docker Image by default
+expected_files = [".", "..", "external.dg", "src", "db", "users.dg"]
+
+# Use external.dg for enter all external files, which you want to add to
+# your Docker image. All files separated by \n (Enter)
+def add_file():
+    buffer = ''
+    if "external.dg" in os.listdir(work_directory):
+        file = open(work_directory+"external.dg", "r")
+        for line in file.readline():
+            buffer += "COPY {} /external".format(line)
+        file.close()
+    else:
+        print("Cannot find 'external.dg' in current directory. Will add all files in this directory to Docker image"
+              " in directory '/external', expecting {}".expected_files)
+        for file in os.listdir(work_directory):
+            if file not in expected_files:
+                buffer += "COPY {} /external".format(file)
+    return buffer
+
+# Use users.dg for enter all users and groups, which you want to add to
+# your Docker image. Format of one note in 'users.dg' <group>:<user>:<password>
+def add_users(root_password):
+    buffer = ''
+    if "users.dg" in os.listdir(work_directory):
+        file = open(work_directory+"users.dg", "r")
+        for line in file.readline():
+            group, user, password = line.split(":")
+            buffer += "RUN groupadd {}\n".format(group)
+            buffer += "RUN useradd -g {} {}\n".format(group, user)
+            buffer += "RUN echo '{}:{}' | chpasswd\n".format(user, password)
+        file.close()
+
+    buffer += "RUN echo 'root:{}' | chpasswd\n".format(root_password)
+    return buffer
+
 # Function for adding ssh-server
 # Based on https://docs.docker.com/engine/examples/running_ssh_service/
-# Have only 'root' user and you can set 'passowrd'
 # But after using this function, please, read https://habrahabr.ru/company/infopulse/blog/237737/
-def sshd_config(install, password):
+def sshd_config(install):
     buffer_string = ''
     buffer_string += "RUN {} openssh-server\n".format(install)
     buffer_string += "RUN mkdir /var/run/sshd\n"
 
-    # TODO: May be change password before sshd_config
-    buffer_string += "RUN echo 'root:{}' | chpasswd\n".format(password)
+    # If in current directory there is sshd_config, copy it
+    # to /var/run/sshd/. Else, create default sshd_config
+    if "sshd_config" in os.listdir(work_directory):
+        buffer_string += "ADD {} /etc/ssh/".format(work_directory+"sshd_config")
+    else:
+        print("Cannot find 'sshd_config' in current directory. Will use default settings.")
 
-    # This string replace all rules in sshd_config
-    # It's setting by default only for testing
-    # I recommend change this default settings in future.
-    buffer_string += "RUN sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config\n"
+        # This string replace all rules in sshd_config
+        # It's setting by default only for testing
+        # I recommend change this default settings in future.
+        buffer_string += "RUN sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config\n"
 
     # For ssh docker will use 22th port
     buffer_string += "EXPOSE 22\nCMD [\"/usr/sbin/sshd\", \"-D\"]\n"
 
     return buffer_string
 
-def telnetd_config(install, password):
+def telnetd_config(install):
     buffer_string = ''
     buffer_string += "RUN {} telnetd\n".format(install)
-
-    # TODO: May be change password before telnetd_config
-    buffer_string += "RUN echo 'root:{}' | chpasswd\n".format(password)
     buffer_string += "EXPOSE 23\nCMD [\"/usr/sbin/sshd\", \"-D\"]\n"
 
     return buffer_string
 
+
+# TODO: Make it a like PAY ATTENTION
 # Function for checking of existence some package in
 # repository of OS
 def check_existence_in_repository(os_name, utils):
 
-    if os_name not in os.listdir(os.path.dirname(__file__) + "./os/"):
+    if os_name not in os.listdir(os.path.dirname(__file__) + "/os/"):
         return None
     # TODO: This checking works only in Unix systems
     # If file less than 3 (grabber.py , ., ..), that list of files in
@@ -100,24 +138,13 @@ def check_existence_in_repository(os_name, utils):
                     datafile.close()
     return False
 
-
-
 def add_external_file(cmd, answer):
     answer = answer.split(" ")
     return ("{} {} {}\n".format(cmd, answer[0], answer[1]))
 
 # This functions for external file adding unification (requests.txt, DB, composer.json, etc.)
-
 def add_external_file_interactive():
-    answer = ''
-
-    cmd = "ADD"
-    while answer.lower() not in "ny":
-        answer = Prompt("This file on local machine? [Y/N]: ").input
-
-    if answer.lower() is "n":
-        cmd = "COPY"
-
+    cmd = "COPY"
     answer = ''
     while not answer and len(answer) != 2:
         answer = Prompt("Please, enter source path (or link) of your file and destination path in your Docker: ").input
@@ -130,7 +157,7 @@ def add_external_file_interactive():
 def language_config(install, language):
     buffer_string = ''
 
-    f = Prompt('Please, enter config file location').input
+    f = "/apps"
     if 'python' in language.lower():
         if "3" in language:
             buffer_string += "RUN {} python3-pip\n".format(install)
@@ -156,6 +183,10 @@ def language_config(install, language):
         # As nodejs's npm make install all references from package.json in current directory
         # we need to enter to this directory and return come back
         buffer_string += "RUN p=pwn && cd {} && npm install && cd $p\n".format(os.path.dirname(f))
+
+    if "c" in language.lower():
+        buffer_string += "RUN {} cmake make\n".format(install)
+
     return buffer_string
 
 def database_interactive(database):
@@ -163,13 +194,7 @@ def database_interactive(database):
     answer = ''
     buffer_string = ''
     if advanced == 1:
-        while answer.lower() not in "yn":
-            answer = Prompt("Is this file on local machine? [Y/N]: ").input
-
-        if answer.lower() is "n":
-            return 0
-        else:
-            files = add_external_file_interactive()
+        files = add_external_file_interactive()
 
         username = Prompt("Please, enter username: ").input
         password = Prompt("Please, enter password: ").input
@@ -179,6 +204,8 @@ def database_interactive(database):
         username = "admin"
         password = "password"
         database_name = "database"
+
+        #TODO: May be change '/db/*' on result of os.listdir()?
         files = ['', '/db/*']
         # https://stackoverflow.com/questions/25920029/setting-up-mysql-and-importing-dump-within-dockerfile
         # https://stackoverflow.com/questions/4546778/how-can-i-import-a-database-with-mysql-from-terminal
@@ -196,7 +223,7 @@ def database_interactive(database):
     if "mongodb" in database.lower():
         buffer_string += "RUN mongorestore {}\n".format(files[1])
 
-    print ("Login: {}\nPassword: {}\nDatabase: {}\n".format(username, password, database.lower()))
+    print ("Login: {}\nPassword: {}\nDatabase: {}\n".format(username, password, database_name))
     return buffer_string
 
 class OSPrompt(Prompt):
@@ -245,7 +272,7 @@ class DatabasePrompt(Prompt):
 
 # Function for choice of specific utils from OS's repository
 def check_existence(os_name, default):
-    if advanced:
+    if not advanced:
         return default
     input_value = Prompt("You can choose default package '{}' (press Enter) or enter your own package: ".format(default)
                          , default=default).input
@@ -281,6 +308,9 @@ class Cargo(CementApp):
 with Cargo() as app:
     buffer_string = ''
     # add arguments to the parser
+    app.args.add_argument('--path', action='store',
+                          help='choice of path to directory with sources')
+
     app.args.add_argument('-o', '--os', action='store',
                           help='choice Operation System')
 
@@ -293,25 +323,26 @@ with Cargo() as app:
     app.args.add_argument('-p', '--ports', action='store',
                           help='choice using ports')
 
-    app.args.add_argument('--path', action='store',
-                          help='choice of path to directory with sources')
+    app.args.add_argument('-s', '--ssh', action='store_true',
+                          help='make active ssh-server' )
 
-    app.args.add_argument('-s', '--ssh', action='store',
-                          help='make active ssh-server and set root\'s password' )
-
-    app.args.add_argument('-t', '--telnet', action='store',
-                          help='make active telnet-server and set root\'s password' )
+    app.args.add_argument('-t', '--telnet', action='store_true',
+                          help='make active telnet-server' )
 
     app.args.add_argument('-r', '--restart', action='store',
                           help='choice container restart condition' )
 
-    app.args.add_argument('-v', '--volume', action='store_true',
+    app.args.add_argument('-v', '--volume', action='store',
                           help='choice common (shared) directory(-ies)' )
 
     app.args.add_argument('-a', '--add', action='store_true',
-                          help='choice volume directory(-ies)' )
+                          help='activation of adding files from \"external.dg\"' )
 
-    app.args.add_argument('-w', '--workdir', action='store_true',
+    app.args.add_argument('-u', '--user', action='store',
+                          help='activation of adding users and group from \"users.dg\"'
+                               'Here you must enter root\'s password')
+
+    app.args.add_argument('-w', '--workdir', action='store',
                           help='choice workdir path' )
 
     app.args.add_argument('--advanced', action='store_true',
@@ -319,8 +350,17 @@ with Cargo() as app:
     # run the application
     app.run()
 
+    if not app.pargs.os:
+        print("Use flag '-o' for point Operation System for your Docker Image")
+        exit(-1)
+    if not app.pargs.language:
+        print("Use flag '-l' for point Language of your service")
+        exit(-1)
+
     if app.pargs.path:
         work_directory = app.pargs.path
+    else:
+        work_directory = os.path.curdir
 
     # TODO: Make checking of OS (if it exist)
     # Choice Operation System and set install
@@ -336,9 +376,9 @@ with Cargo() as app:
         elif "centos" in app.pargs.os.lower():
             install = "yum install"
         else:
-            print("We are cannot support this OS: {}".format(app.pargs.os))
+            print("We are cannot support this OS: {}\n".format(app.pargs.os))
+            exit(-1)
 
-        # TODO: Except choice of unsupport OS. May be via WHILE
         operation_system = app.pargs.os
 
     buffer_string += maintainer
@@ -346,49 +386,61 @@ with Cargo() as app:
     # Make install some language interpretator, independently from OS
     # Language pull: Rust, Go, Ruby, Python, PHP, Javascript, C, C++, Java
     # For Rust: curl https://sh.rustup.rs -sSf | sh
-
     if app.pargs.advanced:
         advanced = 1
 
     if app.pargs.language:
         specify_language = 0
         language_packet = ""
-        app.log.info("Received option: l => {}".format(app.pargs.language.lower()))
+        print("You chosen language '{}'".format(app.pargs.language.lower()))
         if "ruby" in app.pargs.language.lower():
             if "ubuntu" in operation_system or "debian" in operation_system:
                 language_packet = "ruby-full"
             else:
                 language_packet = "ruby"
-        print("You chosen language '{}'".format(app.pargs.language.lower()))
 
-        # Make a choice of version
-        if "python" in app.pargs.language.lower():
+        elif "python3" in app.pargs.language.lower():
+            language_packet = check_existence(operation_system, "python3.5")
+
+        elif "python" in app.pargs.language.lower():
             language_packet = check_existence(operation_system, "python")
-        if "php" in app.pargs.language.lower():
+
+        elif "php" in app.pargs.language.lower():
             language_packet = check_existence(operation_system, "php")
-        if "js" in app.pargs.language.lower():
+
+        elif "js" in app.pargs.language.lower():
             language_packet = check_existence(operation_system, "nodejs")
-        if "c" in app.pargs.language.lower():
+
+        elif "c" in app.pargs.language.lower():
             language_packet = check_existence(operation_system, "gcc")
-        if "c++" in app.pargs.language.lower():
+
+        elif "c++" in app.pargs.language.lower():
             language_packet = check_existence(operation_system, "g++")
-        # Make a choice of interpretator
-        if "java" in app.pargs.language.lower():
+
+        elif "go" in app.pargs.language.lower():
+            language_packet = check_existence(operation_system, "golang")
+
+        elif "java" in app.pargs.language.lower():
             if "ubuntu" in operation_system:
                 language_packet = check_existence(operation_system, "openjdk-9-jre")
             elif "debian" in operation_system:
                 language_packet = check_existence(operation_system, "default-jre")
-        if "rust" in app.pargs.language.lower():
+
+        elif "rust" in app.pargs.language.lower():
             print("This is specify language, which are not in {}'s repository. Default value is used"
                   "".format(operation_system))
             buffer_string += "RUN {} curl\n".format(install)
             buffer_string += "RUN curl https://sh.rustup.rs -sSf | sh\n"
             specify_language = 1
+        else:
+            print("We cannot support language {}".format(app.pargs.language.lower()))
+            exit(-1)
 
         if not specify_language:
             buffer_string += "RUN {} {}\n".format(install, language_packet)
 
         buffer_string += "ADD {} /apps\n".format(work_directory + "/src/")
+        buffer_string += language_config(install, language_packet)
 
     # Make a choice of Databases
     # Pull of DBs: MySQL, PostgreSQL, MongoDB
@@ -400,23 +452,24 @@ with Cargo() as app:
 
         # Make a choice of version
         if "mysql" in app.pargs.database.lower():
-            database = check_existence(operation_system, "mysql-server")
+            database = check_existence(operation_system, database_default["mysql"])
 
         # PostgreSQL have a some dependencies, a like
         # 'postgresql-contrib', which provides additional functionality
-        if "postgresql" in app.pargs.database.lower():
-            database = check_existence(operation_system, "postgresql")
+        elif "postgresql" in app.pargs.database.lower():
+            database = check_existence(operation_system, database_default["postgresql"])
 
-        if "mongodb" in app.pargs.database.lower():
-            database = check_existence(operation_system, "mongodb-server")
+        elif "mongodb" in app.pargs.database.lower():
+            database = check_existence(operation_system, database_default["mongodb"])
 
+        else:
+            print("We cannot support database {}".format(app.pargs.database.lower()))
+            exit(-1)
 
         buffer_string += "RUN {} {}\n".format(install, database)
 
         # Interactive mode for import DB to Docker
-
         buffer_string += database_interactive(database)
-
 
     # Make a choice of ports, using by Docker
     if app.pargs.ports:
@@ -444,11 +497,11 @@ with Cargo() as app:
 
     # Make active ssh-server and setting root's password
     if app.pargs.ssh:
-        buffer_string += sshd_config(install, app.pargs.ssh)
+        buffer_string += sshd_config(install)
 
     # Make active telnet-server and setting root's password
     if app.pargs.telnet:
-        buffer_string += telnetd_config(install, app.pargs.telnet)
+        buffer_string += telnetd_config(install)
 
     # List of docker restart conditions
     restart_conditions = ['on-failure', 'always', 'unless-stopped']
@@ -460,31 +513,19 @@ with Cargo() as app:
 
     # Make a choice of volume directory
     if app.pargs.volume:
-        choice = ''
-
-        # Processing of directory volume choice
-        while choice.lower() != "q":
-            choice = Prompt("Choose next common(shared) directory (or \'Q\' for finish choice): ").input
-            if choice.lower() is "q":
-                continue
-            buffer_string += "VOLUME [\"{}\"]\n".format(choice)
+        if "," in app.pargs.volume:
+            for volume in app.pargs.volume.split(","):
+                buffer_string += "VOLUME [\"{}\"]\n".format(volume)
+        else:
+            buffer_string += "VOLUME [\"{}\"]\n".format(app.pargs.volume)
 
     # Make a choice of addition files to docker image
+    buffer_string += "RUN mkdir /external"
     if app.pargs.add:
-        choice_paths_from = ''
+        buffer_string += add_file()
 
-        # Processing of adding files to image
-        while choice_paths_from.lower() != "q":
-            choice_paths_from = Prompt("Choose next file (or \'Q\' for finish choice): ").input
-            choice_paths_to = Prompt("Choose destination directory: ").input
-            if choice_paths_from.lower() is "q":
-                continue
-
-            if len(choice_paths_from) == 0 or len(choice_paths_to) == 0:
-                print("Wrong parameters!")
-                continue
-
-            buffer_string += "ADD \"{}\" \"{}\"\n".format(choice_paths_from, choice_paths_to)
+    if app.pargs.user:
+        buffer_string += add_users(app.pargs.user)
 
     if app.pargs.workdir:
         buffer_string += "WORKDIR {}\n".format(app.pargs.workdir)
